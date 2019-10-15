5191c828257f95bd0a893a0bf9b9a4648397bb08
Rafael Franke <rafinhamr.13@gmail.com>
2019-10-15 01:06:01 -0400

    Tabs

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container, Top, Logo, Title } from './styles';
+
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/Nubank_Logo.png';
+
+export default function Header() {
+    return (
+        <Container>
+            <Top>
+                <Logo source={logo} />
+                <Title>Franke</Title>
+            </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+        </Container>
+    );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    background: #8b10ae;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    max-height: 400px;
+    justify-content = center;
+    z-index: 5;
+`;
+
+export const Card = styled.View`
+    flex: 1;
+    background: #fff;
+    border-radius: 4px;
+    margin: 0 20px;
+    height: 100%;
+`;
+
+export const CardHeader = styled.View``;
+export const CardContent = styled.View``;
+export const Title = styled.Text``;
+export const Descrption = styled.Text``;
+export const CardFooter = styled.View``;
+export const Annotation = styled.Text``;
