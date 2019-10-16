dee9922320186f10ab1f8c77f05cc741ef5cbb1f
Rafael Franke <rafinhamr.13@gmail.com>
2019-10-16 02:18:27 -0400

    Ajustes na interface

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,77 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 import {
   Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation
 } from './styles';
 
 export default function Main(){
+    let offSet = 0;
+    const translateY = new Animated.Value(0);
+    const animatedEvent = Animated.event(
+        [
+            {
+                nativeEvent: {
+                    translationY: translateY,
+                }
+            }
+        ],
+        { useNativeDriver: true },
+    );
+
+    function onHandlerStateChanged(event) {
+        if ( event.nativeEvent.oldState === State.ACTIVE) {
+            let opened = false;
+
+            const { translationY } = event.nativeEvent;
+            offSet += translationY;
+
+            if ( translationY >= 100 ) {
+                opened = true;
+            } else {
+                translateY.setValue(offSet);
+                translateY.setOffset(0);
+                offSet = 0;
+            }
+
+            Animated.timing(translateY, {
+                toValue: opened ? 380 : 0,
+                duration: 200,
+                useNativeDriver: true,
+            }).start(() => {
+                offSet = opened ? 380 : 0;
+                translateY.setOffset(offSet);
+                translateY.setValue(0);
+            });
+        }
+    }
+
     return (
         <Container>
             <Header />
-
-
             <Content>
-                <Card>
+                <Menu translateY={translateY} />
+
+                <PanGestureHandler
+                    onGestureEvent={animatedEvent}
+                    onHandlerStateChange={onHandlerStateChanged}
+                >
+                    <Card style={{
+                        transform: [{
+                            translateY: translateY.interpolate({
+                                inputRange: [-350, 0, 380],
+                                outputRange: [-50, 0, 380],
+                                extrapolate: 'clamp',
+                            }),
+                        }]
+                    }}>
                     <CardHeader>
                         <Icon name="attach-money" size={28} color="#666" />
                         <Icon name="visibility-off" size={28} color="#666" />
@@ -29,11 +85,11 @@ export default function Main(){
                             Transferência recebida por Rafael Franke
                         </Annotation>
                     </CardFooter>
-                </Card>
+                    </Card>
+                </PanGestureHandler>
             </Content>
 
-
-            <Tabs />
+            <Tabs  translateY={translateY} />
         </Container>
     );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,3 +1,4 @@
+import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -12,17 +13,49 @@ export const Content = styled.View`
     z-index: 5;
 `;
 
-export const Card = styled.View`
+export const Card = styled(Animated.View)`
     flex: 1;
     background: #fff;
     border-radius: 4px;
     margin: 0 20px;
     height: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
 `;
 
-export const CardHeader = styled.View``;
-export const CardContent = styled.View``;
-export const Title = styled.Text``;
-export const Descrption = styled.Text``;
-export const CardFooter = styled.View``;
-export const Annotation = styled.Text``;
+export const CardHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px;
+`;
+
+export const CardContent = styled.View`
+    flex: 1;
+    padding: 0 30px;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    font-size: 13px;
+    color: #999;
+`;
+
+export const Description = styled.Text`
+    font-size: 32px;
+    margin-top: 3px;
+    color: #333;
+
+`;
+export const CardFooter = styled.View`
+    padding: 30px;
+    background: #eee;
+    border-radius: 4px;
+`;
+
+export const Annotation = styled.Text`
+    font-size: 12px;
+    color: #333;
+`;
